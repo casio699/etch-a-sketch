f28e8d07d30e98af8506d5242e30e191ed980cce
ahmad khoder <ahmad.khoder2332@gmail.com>
2023-12-21 18:54:52 +0200

    you can now change resol. prompt user input defines how many pixels there are within a container

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ etch-a-sketch
 16x16 div grid 384x384px 24x24px each
 divs in container using js
 flexbox into a grid using css
+box background color to yellow on hover
+button that controls brush weight by changing how many boxes there is within the same width of the console
--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const container=document.querySelector("#container");
+//variable declarations
+let boxnb=16;
+document.documentElement.style.setProperty('--boxnb', boxnb);
 
-for(i=0;i<(16*16);i++){
-    const div=document.createElement("div");
-    div.classList.add("box")
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor="yellow";
-    })
+const container=document.querySelector("#container");
+//initial creation of console
+createConsole(boxnb);
+
+//brush weight button
+const weight=document.getElementById("weight");
+weight.addEventListener("click", () =>{
+     boxnb=prompt("enter desired number between 10 and 100 of squares inside the console width");
+     document.documentElement.style.setProperty("--boxnb", boxnb);
+     createConsole(boxnb);
+});
+
+//console and on hover
+function createConsole(boxnb){
+    container.innerHTML="";
+    for(i=0;i<(boxnb*boxnb);i++){
+        const div=document.createElement("div");
+        div.classList.add("box")
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor="yellow";
+        });
     container.appendChild(div);
+    }    
 }
+
 
 
